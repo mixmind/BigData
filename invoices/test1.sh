c447ddb2e8c3d923c2235203b2c8b7e77581043b
Reuven St <mixmind@me.com>
2019-08-18 14:11:36 +0300

    update

--- invoices/test1.sh
+++ invoices/test1.sh
@@ -4,6 +4,7 @@
 for i in {1..100}; do
 
 node inv.js > invoice$i.json &&
-curl -X POST  -i  -F file=@invoice$i.json  35.208.177.111:8080/upload; 
+curl -X POST  -i  -F file=@invoice$i.json  35.208.177.111:8080/upload &&
+sleep 5;
 done
 
